e71cc1ae9707d0ef68be90df0c967ae3bb6a63df
shashanknamdeo <110931864+shashanknamdeo@users.noreply.github.com>
2025-06-26 22:16:44 +0530

    Shashank Update

--- DSA/DSAProblems/GoogleSheatProblems.py
+++ DSA/DSAProblems/GoogleSheatProblems.py
@@ -125,9 +125,121 @@ def arrayReverse(array):
 
 def arrayReverse(array):
     """
+    By Swapping Elements - O(n) Time and O(1) Space
+    this program is correct but it has less step
     """
-    n = len()
-    mid = n
-    for i in range(0, ):
+    n = len(array)
+    mid = n // 2 
+    for i in range(0, mid):
+        temp = array[i]
+        array[i] = array[n-i-1]
+        array[n-i-1] = temp
+    # 
+    return array
 
 
+def maxSubArray(self, nums):
+    """
+    find the continous sub array having maximum sum
+    """
+init_flag       = False       # express that is there any item in sub string
+    for item in nums:
+
+
+if init_flag == False:
+    sub_array       = []
+    negetive_sum    = None
+    negative_flag   = False   # express that are we incounterd with a negestive sum which is not normalise by positive sum
+    # 
+    if item < 0 :
+        continue
+    else :
+        init_flag = True
+        sub_array.append(item)
+# 
+else:
+    if negative_flag == False:
+        if item > 0:
+            handelpositiveItem()
+        # 
+        elif item < 0:
+            handelNegativeItem(item=item, negetive_sum=negetive_sum, negative_flag=negative_flag, sub_array=array, init_flag=init_flag)
+    # 
+    if negative_flag == True:
+        handelNegativeItem(item=item, negetive_sum=negetive_sum, negative_flag=negative_flag, sub_array=array, init_flag=init_flag)
+
+
+def handelpositiveItem(item=item, negetive_sum=negetive_sum, negative_flag=negative_flag, sub_array=array, init_flag=init_flag):
+    """
+    """
+    if negative_flag == False and item > 0:
+        sub_array.append(item)
+    # 
+    elif negative_flag == True:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def handelNegativeItem(item, negetive_sum, negative_flag, sub_array, init_flag):
+    """
+    """
+    if negative_flag == False :
+        if sub_array.sum() + item >= 0 :
+            sub_array.append(item)
+            negative_flag = True
+            negetive_sum = item
+        else:
+            init_flag = False
+            return
+    # 
+    else:
+        if sub_array.sum() + item < 0:
+            init_flag = False
+            return
+        else:
+
+
+
+
+
+1,2,-2,1,-1
+
+
+
+# devide and conquer menthord
+#     divide the arrray in single element substring
+#     if add subarray  to other is make the the sum grater than add it
+#     otherwise left it
+
+
+def maxSubArray(self, nums):
+    """
+    find the continous sub array having maximum sum
+    Divide and Conquer
+    """
+
+
+
+
+def getLargestSumSubarray(array):
+    """
+    """
+    # divide
+    
+    # base case
+    if 
+
+
+hashing
