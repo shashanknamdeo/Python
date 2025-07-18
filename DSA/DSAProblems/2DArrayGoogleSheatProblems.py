3faebe39b11a91fa23b73feed056d18c9ee508b7
shashanknamdeo <110931864+shashanknamdeo@users.noreply.github.com>
2025-07-18 11:52:59 +0530

    Shashank Update

--- DSA/DSAProblems/2DArrayGoogleSheatProblems.py
+++ DSA/DSAProblems/2DArrayGoogleSheatProblems.py
@@ -55,83 +55,109 @@
 #     return matrix
 
 
-def spiralOrder(self, matrix):
-        """
-        """
-        total_row = len(matrix)
-        total_col = len(matrix[0])
-        # 
-        traverse_list = []
-        # 
-        row_no = 0
-        col_no = 0
-        while (row_no < total_row//2 ) and (col_no < total_col//2):
-            traverse_list += spiralTraverse(matrix=matrix, row_no=row_no, col_no=col_no, total_row=total_row, total_col=total_col)
-            row_no += 1
-            col_no += 1
-        # 
-        if total_row % 2 + total_col % 2 == 1:
-            if total_row % 2 == 1:
-                for i in range(col_no, total_col - col_no):
-                    # print(1, 'row', i)
-                    traverse_list.append(matrix[row_no][i])
-            # 
-            elif total_col % 2 == 1:
-                for j in range(row_no, total_row - row_no):
-                    # print(1, 'col', j)
-                    traverse_list.append(matrix[j][col_no])
-        # 
-        if total_row % 2 + total_col % 2 == 2:
-            # print(2)
-            if total_row <= total_col:
-                for i in range(col_no, total_col - col_no):
-                    traverse_list.append(matrix[row_no][i])
-            # 
-            else:
-                for j in range(row_no, total_row - row_no):
-                    traverse_list.append(matrix[j][col_no])
-        # 
-        return traverse_list
+# def spiralOrder(matrix):
+#         """
+#         """
+#         total_row = len(matrix)
+#         total_col = len(matrix[0])
+#         # 
+#         traverse_list = []
+#         # 
+#         row_no = 0
+#         col_no = 0
+#         while (row_no < total_row//2 ) and (col_no < total_col//2):
+#             traverse_list += spiralTraverse(matrix=matrix, row_no=row_no, col_no=col_no, total_row=total_row, total_col=total_col)
+#             row_no += 1
+#             col_no += 1
+#         # 
+#         if total_row % 2 + total_col % 2 == 1:
+#             if total_row % 2 == 1:
+#                 for i in range(col_no, total_col - col_no):
+#                     # print(1, 'row', i)
+#                     traverse_list.append(matrix[row_no][i])
+#             # 
+#             elif total_col % 2 == 1:
+#                 for j in range(row_no, total_row - row_no):
+#                     # print(1, 'col', j)
+#                     traverse_list.append(matrix[j][col_no])
+#         # 
+#         if total_row % 2 + total_col % 2 == 2:
+#             # print(2)
+#             if total_row <= total_col:
+#                 for i in range(col_no, total_col - col_no):
+#                     traverse_list.append(matrix[row_no][i])
+#             # 
+#             else:
+#                 for j in range(row_no, total_row - row_no):
+#                     traverse_list.append(matrix[j][col_no])
+#         # 
+#         return traverse_list
 
 
-def spiralTraverse(matrix, row_no, col_no, total_row, total_col):
-    """
-    """
-    current_row = row_no
-    current_col = col_no
-    traverse_list = []
-    # 
-    traverse_list += matrix[current_row][current_col:total_col - current_col]
-    current_col = total_col - current_col - 1
-    # 
-    j = current_row + 1
-    while j < total_row - current_row:
-        traverse_list.append(matrix[j][current_col])
-        j += 1
-    current_row = j - 1
-    # 
-    temp_array = matrix[current_row][col_no:current_col]
-    temp_array.reverse()
-    traverse_list += temp_array
-    # 
-    j = current_row - 1
-    while j > row_no:
-        traverse_list.append(matrix[j][col_no])
-        j -= 1
-    # 
-    return traverse_list
+# def spiralTraverse(matrix, row_no, col_no, total_row, total_col):
+#     """
+#     """
+#     current_row = row_no
+#     current_col = col_no
+#     traverse_list = []
+#     # 
+#     traverse_list += matrix[current_row][current_col:total_col - current_col]
+#     current_col = total_col - current_col - 1
+#     # 
+#     j = current_row + 1
+#     while j < total_row - current_row:
+#         traverse_list.append(matrix[j][current_col])
+#         j += 1
+#     current_row = j - 1
+#     # 
+#     temp_array = matrix[current_row][col_no:current_col]
+#     temp_array.reverse()
+#     traverse_list += temp_array
+#     # 
+#     j = current_row - 1
+#     while j > row_no:
+#         traverse_list.append(matrix[j][col_no])
+#         j -= 1
+#     # 
+#     return traverse_list
 
 
 
 def rotate(matrix):
     """
     make 4 array
-    ht_array
-    hb_array
-    vl_array
-    vr_array
     """
+    top = left = 0
+    bottom = right = len(matrix) - 1
+    # 
+    while top < bottom:
+        ht_array = matrix[top]
+        hb_array = matrix[bottom]
+        # print(ht_array, hb_array)
+        # 
+        vl_array = []
+        vr_array = []
+        for i in range(top, bottom+1):
+            # 
+            vl_array.append(matrix[i][left])
+            matrix[i][left] = hb_array[i]
+            # 
+            vr_array.append(matrix[i][right])
+            matrix[i][right] = ht_array[i]
+            print(vl_array, vr_array)
+        # 
+        vl_array.reverse()
+        vr_array.reverse()
+        matrix[top][left:right+1] = vl_array
+        matrix[bottom][left:right+1] = vr_array
+        # 
+        top = left = top + 1
+        bottom = right = bottom - 1
+    # 
+    return matrix
 
+
+test remaining
 
 
 
