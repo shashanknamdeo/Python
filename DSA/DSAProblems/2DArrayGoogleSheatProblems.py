17a96f726e595283cc9c6b6408650682aa4dd3cf
shashanknamdeo <110931864+shashanknamdeo@users.noreply.github.com>
2025-07-20 10:27:12 +0530

    Shashank Update

--- DSA/DSAProblems/2DArrayGoogleSheatProblems.py
+++ DSA/DSAProblems/2DArrayGoogleSheatProblems.py
@@ -1,3 +1,6 @@
+
+# https://docs.google.com/spreadsheets/d/1hXserPuxVoWMG9Hs7y8wVdRCJTcj3xMBAEYUOXQ5Xag/edit?gid=0#gid=0
+
 # def diagonalTraversal(matrix):
 #     """
 #     """
@@ -123,61 +126,134 @@
 
 
 
-def rotate(matrix):
+# def rotate(matrix):
+#     """
+#     make 4 array
+#     # 
+#     another Approach:
+#     step 1. Transpose the matrix (swap rows with columns).
+#     step 2. Reverse each row.
+#     """
+#     top = left = 0
+#     bottom = right = len(matrix) - 1
+#     # 
+#     while top < bottom:
+#         ht_array = matrix[top]
+#         hb_array = matrix[bottom]
+#         # print(ht_array, hb_array)
+#         # 
+#         vl_array = []
+#         vr_array = []
+#         for i in range(top, bottom+1):
+#             # 
+#             vl_array.append(matrix[i][left])
+#             matrix[i][left] = hb_array[i]
+#             # 
+#             vr_array.append(matrix[i][right])
+#             matrix[i][right] = ht_array[i]
+#             print(vl_array, vr_array)
+#         # 
+#         vl_array.reverse()
+#         vr_array.reverse()
+#         matrix[top][left:right+1] = vl_array
+#         matrix[bottom][left:right+1] = vr_array
+#         # 
+#         top = left = top + 1
+#         bottom = right = bottom - 1
+#     # 
+#     return matrix
+
+
+# def exist(board, word):
+#     """
+#     https://leetcode.com/problems/word-search/
+#     solve by advance topic like DP and back tracking
+#     """
+
+
+# def numberOfIslands():
+#     """
+#     https://www.geeksforgeeks.org/dsa/find-the-number-of-islands-using-dfs/
+#     solve by advance topic like DFS
+#     """
+
+
+# def replaceOWithX():
+#     """
+#     https://www.geeksforgeeks.org/dsa/given-matrix-o-x-replace-o-x-surrounded-x/
+#     """
+
+
+# def findCommonElement(matrix):
+#     """
+#     my idea - implemanted by ChatGPT
+#     time complexity - O(n*m)
+#     """
+#     m = len(matrix)
+#     n = len(matrix[0])
+#     # 
+#     # Initialize a pointer for each row (start from column 0)
+#     indices = [0] * m
+#     # 
+#     # Initialize max_num as the first element of the first row
+#     max_num = matrix[0][0]
+#     # 
+#     while True:
+#         count_equal = 0  # Counts how many rows are currently at max_num
+#         # 
+#         for i in range(m):
+#             # Move pointer forward if current element is less than max_num
+#             while indices[i] < n and matrix[i][indices[i]] < max_num:
+#                 indices[i] += 1
+#             # 
+#             # If we moved out of bounds → no common element
+#             if indices[i] == n:
+#                 return -1
+#             # 
+#             # If current element is greater than max_num → update max_num and restart checking
+#             if matrix[i][indices[i]] > max_num:
+#                 max_num = matrix[i][indices[i]]
+#                 break  # Restart loop with updated max_num
+#             # 
+#             # If current element equals max_num
+#             if matrix[i][indices[i]] == max_num:
+#                 count_equal += 1
+#         # 
+#         # If all rows are pointing to max_num → we found the common element
+#         if count_equal == m:
+#             return max_num
+
+
+def CreateMatrixWithAlternatingRectanglesOfOAndX(rows, columns):
     """
-    make 4 array
-    # 
-    another Approach:
-    step 1. Transpose the matrix (swap rows with columns).
-    step 2. Reverse each row.
     """
-    top = left = 0
-    bottom = right = len(matrix) - 1
+    matrix = [['X' for _ in range(columns)] for _ in range(rows)]
+    # print(matrix)
     # 
-    while top < bottom:
-        ht_array = matrix[top]
-        hb_array = matrix[bottom]
-        # print(ht_array, hb_array)
+    top = left = 1
+    bottom = rows - 2
+    right = columns - 2
+    # 
+    while top <= bottom and left <= right:
+        matrix[top][left:right+1] = ['O' for _ in range(left, right+1)]
         # 
-        vl_array = []
-        vr_array = []
-        for i in range(top, bottom+1):
-            # 
-            vl_array.append(matrix[i][left])
-            matrix[i][left] = hb_array[i]
-            # 
-            vr_array.append(matrix[i][right])
-            matrix[i][right] = ht_array[i]
-            print(vl_array, vr_array)
+        if top + 1 <= bottom - 1:
+            for i in range(top+1, bottom):
+                matrix[i][left] = matrix[i][right] = 'O'
         # 
-        vl_array.reverse()
-        vr_array.reverse()
-        matrix[top][left:right+1] = vl_array
-        matrix[bottom][left:right+1] = vr_array
+        matrix[bottom][left:right+1] = ['O' for _ in range(left, right+1)]
         # 
-        top = left = top + 1
-        bottom = right = bottom - 1
+        top += 2
+        left += 2
+        bottom -= 2
+        right -= 2
+    # 
+    # import pprint
+    # pprint.pprint(matrix)
     # 
     return matrix
 
 
-def exist(board, word):
-    """
-    https://leetcode.com/problems/word-search/
-    solve by advance topic like DP and back tracking
-    """
-
-
-def numberOfIslands():
-    """
-    https://www.geeksforgeeks.org/dsa/find-the-number-of-islands-using-dfs/
-    solve by advance topic like DFS
-    """
-
-def replaceOWithX():
-    """
-    https://www.geeksforgeeks.org/dsa/given-matrix-o-x-replace-o-x-surrounded-x/
-    """
 
 
 
@@ -189,8 +265,30 @@ def replaceOWithX():
 
 
 
-matrix = [[1,2,3],[4,5,6],[7,8,9]]
-# Output: [[7,4,1],[8,5,2],[9,6,3]]
 
-print(rotate(matrix=matrix))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+rows = 10
+columns = 5
+
+print(CreateMatrixWithAlternatingRectanglesOfOAndX(rows=rows, columns=columns))
 # print(spiralTraverse(matrix=matrix, row_no=0, col_no=0, total_row=3, total_col=4))
