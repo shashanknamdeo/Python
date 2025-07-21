cc9187c0a9a531295d4fc612a95efd9708a1d14c
shashanknamdeo <110931864+shashanknamdeo@users.noreply.github.com>
2025-07-21 10:54:11 +0530

    Shashank Update

--- DSA/DSAProblems/2DArrayGoogleSheatProblems.py
+++ DSA/DSAProblems/2DArrayGoogleSheatProblems.py
@@ -224,71 +224,84 @@
 #             return max_num
 
 
-def CreateMatrixWithAlternatingRectanglesOfOAndX(rows, columns):
-    """
-    """
-    matrix = [['X' for _ in range(columns)] for _ in range(rows)]
-    # print(matrix)
-    # 
-    top = left = 1
-    bottom = rows - 2
-    right = columns - 2
-    # 
-    while top <= bottom and left <= right:
-        matrix[top][left:right+1] = ['O' for _ in range(left, right+1)]
-        # 
-        if top + 1 <= bottom - 1:
-            for i in range(top+1, bottom):
-                matrix[i][left] = matrix[i][right] = 'O'
-        # 
-        matrix[bottom][left:right+1] = ['O' for _ in range(left, right+1)]
-        # 
-        top += 2
-        left += 2
-        bottom -= 2
-        right -= 2
-    # 
-    # import pprint
-    # pprint.pprint(matrix)
-    # 
-    return matrix
+# def CreateMatrixWithAlternatingRectanglesOfOAndX(rows, columns):
+#     """
+#     """
+#     matrix = [['X' for _ in range(columns)] for _ in range(rows)]
+#     # print(matrix)
+#     # 
+#     top = left = 1
+#     bottom = rows - 2
+#     right = columns - 2
+#     # 
+#     while top <= bottom and left <= right:
+#         matrix[top][left:right+1] = ['O' for _ in range(left, right+1)]
+#         # 
+#         if top + 1 <= bottom - 1:
+#             for i in range(top+1, bottom):
+#                 matrix[i][left] = matrix[i][right] = 'O'
+#         # 
+#         matrix[bottom][left:right+1] = ['O' for _ in range(left, right+1)]
+#         # 
+#         top += 2
+#         left += 2
+#         bottom -= 2
+#         right -= 2
+#     # 
+#     # import pprint
+#     # pprint.pprint(matrix)
+#     # 
+#     return matrix
 
 
+# def maxArea(matrix):
+#     """
+#     https://www.geeksforgeeks.org/dsa/maximum-size-rectangle-binary-sub-matrix-1s/
+#     2 aproches:
+#         backtracking
+#         histogram area - https://www.geeksforgeeks.org/dsa/largest-rectangular-area-in-a-histogram-using-stack/
+#     using histogram approch
+#     """
+#     def maxHistogramArea(array, len_array):
+#         """
+#         https://www.geeksforgeeks.org/dsa/largest-rectangular-area-in-a-histogram-using-stack/
+#         """
+#         last_smaller = [-1]*len_array
+#         next_smaller = [len_array]*len_array
+#         # 
+#         stack = []
+#         for i in range(0,len_array):
+#             while stack and array[stack[-1]] > array[i]:
+#                 next_smaller[stack.pop()] = i
+#             # 
+#             if stack:
+#                 last_smaller[i] = stack[-1]
+#             # 
+#             stack.append(i)
+#         # 
+#         max_area = 0
+#         for i in range(0, len_array):
+#             width = next_smaller[i] - last_smaller[i] - 1
+#             current_area = array[i]*width
+#             max_area = max(max_area, current_area)
+#         # 
+#         return max_area
+#     # 
+#     rows = len(matrix)
+#     columns = len(matrix[0])
+#     # 
+#     max_area = 0
+#     histogram_array = [0]*columns
+#     for row in matrix:
+#         for i in range(0, columns):
+#             if row[i] == 1:
+#                 histogram_array[i] += 1
+#             else:
+#                 histogram_array[i] = 0
+#         # 
+#         current_area = maxHistogramArea(array=histogram_array, len_array=columns)
+#         max_area = max(max_area, current_area)
+#     # 
+#     return max_area
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-rows = 10
-columns = 5
-
-print(CreateMatrixWithAlternatingRectanglesOfOAndX(rows=rows, columns=columns))
-# print(spiralTraverse(matrix=matrix, row_no=0, col_no=0, total_row=3, total_col=4))
