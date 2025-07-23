f52d78a27a9df189ba866f9bce783d8bf2c02c56
shashanknamdeo <110931864+shashanknamdeo@users.noreply.github.com>
2025-07-23 11:00:15 +0530

    Shashank Update

---
+++ DSA/Array.py
@@ -0,0 +1,33 @@
+# ARRAY
+
+# methord 1
+import array
+array.array('i', [1, 2, 3, 4])
+
+# methord 2
+import numpy as np
+# 1D array
+arr = np.array([10, 20, 30, 40])
+# 2D array
+matrix = np.array([[1, 2], [3, 4]])
+
+# Note : numpy array is best
+
+"""
+
+common Array problem :
+
+1.  Find Largest and Smallest Element
+2.  Reverse the Array
+3.  Kth Largest/Smallest Element
+4.  Sort 0s, 1s, and 2s (Dutch National Flag Problem)
+5.  Move All Zeros to End
+6.  Kadane’s Algorithm (Maximum Subarray Sum)
+7.  Two Sum Problem
+8.  Remove Duplicates from Sorted Array
+9.  Left/Right Rotate Array
+10. Leaders in an Array
+
+"""
+
+# _____________________________________________________________________________________________
