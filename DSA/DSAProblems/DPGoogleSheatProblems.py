7a931a77de034642268d48333d2436481c406b0c
shashanknamdeo <110931864+shashanknamdeo@users.noreply.github.com>
2025-10-25 19:54:18 +0530

    Shashank Update

--- DSA/DSAProblems/DPGoogleSheatProblems.py
+++ DSA/DSAProblems/DPGoogleSheatProblems.py
@@ -3,6 +3,9 @@
 # https://www.geeksforgeeks.org/problems/wildcard-string-matching1126/1
 # https://www.geeksforgeeks.org/problems/word-wrap1646/1
 
+# BT Problem
+# https://www.geeksforgeeks.org/problems/subset-sum-problem2014/1
+
 def nearest(self, grid):
     """
     https://www.geeksforgeeks.org/problems/distance-of-nearest-cell-having-1-1587115620/1
--- DSA/DSAProblems/GreedyGoogleSheatProblems.py
+++ DSA/DSAProblems/GreedyGoogleSheatProblems.py
@@ -2,4 +2,79 @@ def startStation(self, gas, cost):
     """
     https://www.geeksforgeeks.org/problems/circular-tour-1587115620/1
     """
-    
+
+# -------------------------------------------------------------------------------------------------
+
+def activitySelection(start, finish):
+    """
+    """
+    activity = sorted(zip(start, finish), key=lambda x: x[1])
+    # 
+    max_activity = 1
+    prev_finish = activity[0][1]
+    # 
+    for s, f in activity[1:]:
+        if s > prev_finish:
+            max_activity += 1
+            prev_finish = f
+    # 
+    return max_activity
+
+
+# -------------------------------------------------------------------------------------------------
+
+def findMin(n):
+    """
+    """
+    coins = [1,2,5,10]
+    coin = coins.pop()
+    number = 0
+    # 
+    while n != 0:
+        if coin <= n:
+            n -= coin
+            number += 1
+        else:
+            coin = coins.pop()
+    # 
+    return number
+
+
+# -------------------------------------------------------------------------------------------------
+
+def minSum(arr, n):
+    """
+    """
+    if n == 0:
+        return 0
+    elif n == 1:
+        return array[0]
+    # 
+    array.sort()
+    array.reverse()
+    f = ''
+    s = ''
+    # 
+    flag = True
+    while array:
+        if flag == True:
+            f += str(array.pop())
+        else:
+            s += str(array.pop())
+        flag = not flag
+    # 
+    return int(f) + int(s)
+
+
+# array = [6, 8, 4, 5, 2, 3]
+
+array = [5, 3, 0, 7, 4]
+
+
+
+
+
+
+print(minSum(arr=array, n=len(array)))
+
+# df = activitySelection(start, finish)
