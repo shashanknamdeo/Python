1c43b931a3354663baefb903494bb017886b0248
shashanknamdeo <110931864+shashanknamdeo@users.noreply.github.com>
2025-10-27 13:20:36 +0530

    Shashank Update

--- DSA/DSAProblems/GreedyGoogleSheatProblems.py
+++ DSA/DSAProblems/GreedyGoogleSheatProblems.py
@@ -66,15 +66,139 @@ def minSum(arr, n):
     return int(f) + int(s)
 
 
-# array = [6, 8, 4, 5, 2, 3]
+# -------------------------------------------------------------------------------------------------
 
-array = [5, 3, 0, 7, 4]
+def findMinSum(array_1,array_2,n):
+    """
+    """
+    array_1.sort()
+    array_2.sort()
+    # 
+    # print(array_1, array_2)
+    min_sum = 0
+    for i in range(0, n):
+        min_sum += abs(array_1[i] - array_2[i])
+        # print(min_sum)
+    # 
+    return min_sum
+
+
+# -------------------------------------------------------------------------------------------------
+
+def minCost(coin, n, k): 
+    """
+    """
+    coin.sort()
+    # 
+    price = 0
+    # 
+    while coin:
+        price += coin.pop(0)
+        temp = k
+        while temp and coin:
+            coin.pop()
+            temp -= 1
+    # 
+    return price
+
+
+# -------------------------------------------------------------------------------------------------
+
+def nextStack(stack_number):
+    stack_number += 1
+    if stack_number == 4:
+        return 1
+    if stack_number == 5:
+        return 2
+    return stack_number
+
+
+# def maxEqualSum(n1, n2, n3, s1, s2 , s3):
+#     """
+#     """
+#     # 
+#     def stackOperation(stack_number, stack_dict):
+#         stack_number = nextStack(stack_number=stack_number)
+#         stack_dict[stack_number][1] += stack_dict[stack_number][0].pop()
+#         return stack_number, stack_dict
+#     # 
+#     stack_number = 0
+#     max_equal_sum = 0
+#     stack_dict = {1 : [s1, 0], 2 : [s2, 0], 3 : [s3, 0]}
+#     # 
+#     while s1 or s2 or s3:
+#         stack_number, stack_dict = stackOperation(stack_number=stack_number, stack_dict=stack_dict)
+#         print(stack_number)
+#         # 
+#         if stack_dict[1][1] == stack_dict[2][1] and stack_dict[1][1] == stack_dict[3][1]:
+#             max_equal_sum = stack_dict[1][1]
+#         # 
+#         if stack_dict[stack_number][1] >= stack_dict[nextStack(stack_number)][1] and stack_dict[stack_number][1] >= stack_dict[nextStack(stack_number+1)][1]:
+#             continue
+#         # 
+#         stack_number -= 1
+#     # 
+#     return max_equal_sum
+
+
+def maxEqualSum(n1, n2, n3, s1, s2 , s3):
+    """
+    """
+    print(n1, n2, n3, s1, s2 , s3)
+    if not s1 or not  s2 or not  s3:
+        return 0
+    # 
+    temp_max = s1.pop()
+    max_equal_sum = 0
+    stack_number = 1
+    stack_dict = {1 : [s1, temp_max], 2 : [s2, 0], 3 : [s3, 0]}
+    # 
+    while s1 or s2 or s3:
+        stack_number = nextStack(stack_number)
+        stack       = stack_dict[stack_number][0]
+        stack_sum   = stack_dict[stack_number][1]
+        print(temp_max, stack_number, stack, stack_sum)
+        # 
+        if stack and (stack_sum < temp_max or (stack_dict[1][1] == stack_dict[2][1] and stack_dict[1][1] == stack_dict[3][1])):
+            stack_sum += stack.pop()
+            temp_max = max(temp_max, stack_sum)
+            stack_dict[stack_number][0] = stack       
+            stack_dict[stack_number][1] = stack_sum   
+        # 
+        elif not stack and stack_sum < temp_max:
+            print('break : ', stack_number)
+            break
+        # 
+        if stack_dict[1][1] == stack_dict[2][1] and stack_dict[1][1] == stack_dict[3][1]:
+            max_equal_sum = stack_dict[1][1]
+
+        # 
+    # 
+    print(stack_dict)
+    return max_equal_sum
 
 
 
 
 
 
-print(minSum(arr=array, n=len(array)))
+
+
+
+
+s1 = [3, 4]
+s2 = [1, 1, 5]
+s3 = [7]
+
+
+
+n1=n2=n3=0
+
+print(maxEqualSum(n1, n2, n3, s1, s2 , s3))
 
 # df = activitySelection(start, finish)
+
+def maxLevel(boxes, n):
+    """
+    https://www.geeksforgeeks.org/dsa/find-maximum-height-pyramid-from-the-given-array-of-objects/
+    """
