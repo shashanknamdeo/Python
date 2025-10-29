b59d769e3ea09e61d0b68410ec72ddbc1027d30f
shashanknamdeo <110931864+shashanknamdeo@users.noreply.github.com>
2025-10-29 11:08:11 +0530

    Shashank Update

---
+++ GenAI/StartOpenAI.py
@@ -0,0 +1,38 @@
+
+from openai import OpenAI
+
+# client = OpenAI(
+#     base_url="https://models.github.ai/inference",
+#     api_key=)
+
+
+# # messages = [{"role": "system", "content": "You are a friendly AI assistant."}]
+
+# print("Chat with GPT! Type 'quit' to stop.\n")
+
+# user_input = input("You: ")
+# # if user_input.lower() in {"quit", "exit"}:
+# #     break
+
+# messages = [{"role": "user", "content": user_input}]
+
+# response = client.chat.completions.create(
+#     model="gpt-4o-mini",  # or gpt-4.1, gpt-4o, etc.
+#     messages=messages
+# )
+# print(response)
+# reply = response.choices[0].message.content
+# print("Assistant:", reply, "\n")
+
+# messages.append({"role": "assistant", "content": reply})
+
+# from openai import OpenAI
+# client = OpenAI()
+
+# response = client.responses.create(
+#     model="gpt-4o-mini",
+#     input="Write a one-sentence bedtime story about a unicorn."
+# )
+
+# print(response)
+
